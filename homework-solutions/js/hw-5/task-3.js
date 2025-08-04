d06895085b3c08923853918c4d09d0a19ee28351
jorni_pam <acapricorn6@gmail.com>
2025-08-04 22:21:21 +0400

    add the solution hw-5 task-3

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -1,4 +1,4 @@
-/**
+ /**
  * Создать строку с информацией о количестве гласных и согласных букв в слове.
  * Переменная `word` уже создана и содержит строку со словом.
  * Ожидаемый результат для `hello`: "hello contains 2 vowels and 3 consonants".
@@ -6,5 +6,16 @@
 const word = 'hello';
 
 let vowelsAndConsonantsResult = '';
+let vowels = "aeyuio"
+vowels = vowels.split("")
+let vowelsCount = 0;
+let consonantsCount = 0;
+for (let char of word.split("")){
+  if (vowels.includes(char)){
+    vowelsCount ++
+  }
+  else {consonantsCount ++}
+}
+console.log(`hello contains ${vowelsCount} vowels and ${consonantsCount} consonants`)
 
 export { vowelsAndConsonantsResult };
