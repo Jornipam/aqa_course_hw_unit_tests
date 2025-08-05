267e8a3d85d86b3f89c2456b80c01075da406684
jorni_pam <acapricorn6@gmail.com>
2025-08-06 02:19:41 +0400

    add the solution hw-6 task-3

--- homework-solutions/js/hw-6/task-3.mjs
+++ homework-solutions/js/hw-6/task-3.mjs
@@ -7,7 +7,10 @@
   Присвойте результат в переменную "unique"
 */
 let unique;
+
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
 
+unique = [...new Set (arr)];
+console.log(unique)
 
 export { unique };
