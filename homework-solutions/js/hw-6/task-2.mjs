79be626f52eedd4335840449e22512e048a6098a
jorni_pam <acapricorn6@gmail.com>
2025-08-06 01:50:31 +0400

    add the solution hw-6 task-2

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -7,13 +7,54 @@
   Скрипт не должен зависеть от регистра, в котором указаны названия пицц у вас и конкурента
   Воспользуйтесь наборами пицц, что приведены ниже.
 
-  Пиццы:
+  Пиццы:*/
   const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
   const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
-*/
 
-let resultUnique;
+let resultUnique = [];
 let resultNull;
+
+const competitorPizzasLowerCase = [];
+const myPizzasТ1LowerCase = [];
+const myPizzasТ2LowerCase = [];
+
+//приведение к нижнему регистру массивов
+for (let nestedArr of competitorPizzas){
+  nestedArr = nestedArr.toLowerCase()
+  competitorPizzasLowerCase.push(nestedArr)
+};
+
+for (let nestedArr of myPizzasT1){
+  nestedArr = nestedArr.toLowerCase()
+    myPizzasТ1LowerCase.push(nestedArr)
+};
+
+for (let nestedArr of myPizzasT2){
+  nestedArr = nestedArr.toLowerCase()
+    myPizzasТ2LowerCase.push(nestedArr)
+};
+
+// сравнение  моего массива с конкурентом в поиске уникальных пицц 
+for (const nestedArr of myPizzasТ1LowerCase){
+  if (!competitorPizzasLowerCase.includes(nestedArr)){
+    resultUnique.push(nestedArr)
+  }
+  else if (competitorPizzasLowerCase.includes(nestedArr)){
+    resultNull = null
+  }
+};
+
+for (const nestedArr of myPizzasТ2LowerCase){
+  if (!competitorPizzasLowerCase.includes(nestedArr)){
+    resultUnique.push(nestedArr)
+  }
+  else if (competitorPizzasLowerCase.includes(nestedArr)){
+    resultNull = null
+  }
+};
+
+console.log(resultUnique)
+console.log(resultNull)
 
 export { resultNull, resultUnique };
